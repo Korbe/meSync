f57c8787d5432bb791bbd5aa87f5cffc7501bfff
Lukas Korbitsch <korbitschl@gmail.com>
2025-02-09 17:16:35 +0100

    Added second primary layout with sidebar and header, including styles

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import { ref } from 'vue';
+    import Sidebar from './Sidebar/Sidebar.vue';
+    import Header from './Header/Header.vue';
+
+    const sidebarOpen = ref(false);
+
+</script>
+<template>
+    
+    <div class="font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400 flex h-[100dvh] overflow-hidden">
+  
+      <!-- Sidebar -->
+      <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
+  
+      <!-- Content area -->
+      <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        
+        <!-- Site header -->
+        <Header :sidebarOpen="sidebarOpen" @toggle-sidebar="sidebarOpen = !sidebarOpen" />
+
+        <main class="grow">
+            <slot />
+        </main>
+  
+      </div> 
+  
+    </div>
+  </template>
---
+++ resources/js/Layouts/Sidebar/Sidebar.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="min-w-fit">
+    <!-- Sidebar backdrop (mobile only) -->
+    <div class="fixed inset-0 bg-gray-900 z-40 lg:hidden lg:z-auto transition-opacity duration-200" :class="sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'" aria-hidden="true"></div>
+
+    <!-- Sidebar -->
+    <div
+      id="sidebar"
+      ref="sidebar"
+      class="flex lg:flex! flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:w-64! shrink-0 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out"
+      :class="['rounded-r-2xl lg:rounded-none shadow-xs', sidebarOpen ? 'translate-x-0' : '-translate-x-64']"
+    >
+
+      <!-- Sidebar header -->
+      <div class="flex justify-between mb-10 pr-3 sm:px-2">
+        <!-- Close button -->
+        <button
+          ref="trigger"
+          class="lg:hidden text-gray-500 hover:text-gray-400"
+          @click.stop="$emit('close-sidebar')"
+          aria-controls="sidebar"
+          :aria-expanded="sidebarOpen"
+        >
+          <span class="sr-only">Close sidebar</span>
+          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
+          </svg>
+        </button>
+        <!-- Logo -->
+        <Link class="block" href="/">
+          <img src="/images/logo/android-icon-144x144.png" class="h-8" />
+        </Link>
+      </div>
+
+      <!-- Links -->
+      <div class="space-y-8">
+        <!-- Pages group -->
+        <div>
+          <h3 class="text-xs uppercase text-gray-400 dark:text-gray-500 font-semibold pl-3">
+            <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
+            <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
+          </h3>
+          <ul class="mt-3">
+                  
+          </ul>
+        </div>
+      </div>
+
+      <!-- Expand / collapse button -->
+      <div class="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
+        <div class="w-12 pl-4 pr-3 py-2">
+          <button class="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400" @click.prevent="sidebarExpanded = !sidebarExpanded">
+            <span class="sr-only">Expand / collapse sidebar</span>
+            <svg class="shrink-0 fill-current text-gray-400 dark:text-gray-500 sidebar-expanded:rotate-180" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                <path d="M15 16a1 1 0 0 1-1-1V1a1 1 0 1 1 2 0v14a1 1 0 0 1-1 1ZM8.586 7H1a1 1 0 1 0 0 2h7.586l-2.793 2.793a1 1 0 1 0 1.414 1.414l4.5-4.5A.997.997 0 0 0 12 8.01M11.924 7.617a.997.997 0 0 0-.217-.324l-4.5-4.5a1 1 0 0 0-1.414 1.414L8.586 7M12 7.99a.996.996 0 0 0-.076-.373Z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted, watch } from 'vue'
+
+import SidebarLinkGroup from './SidebarLinkGroup.vue'
+
+export default {
+  name: 'Sidebar',
+  props: [
+    'sidebarOpen',
+  ],
+  components: {
+    SidebarLinkGroup,
+  },  
+  setup(props, { emit }) {
+
+    const trigger = ref(null)
+    const sidebar = ref(null)
+
+    const storedSidebarExpanded = localStorage.getItem('sidebar-expanded')
+    const sidebarExpanded = ref(storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true')
+
+    const currentRoute = {
+        name: '',         // Default to null for route name
+        path: './',         // Default to './' for the full path
+        hash: '',           // Default to an empty string for hash
+        query: {},          // Default to an empty object for query parameters
+        params: {},         // Default to an empty object for dynamic route parameters
+        fullPath: './',     // Default to './' for the full URL path
+        matched: [],        // Default to an empty array for matched route records
+  };
+
+    // close on click outside
+    const clickHandler = ({ target }) => {
+      if (!sidebar.value || !trigger.value) return
+      if (
+        !props.sidebarOpen ||
+        sidebar.value.contains(target) ||
+        trigger.value.contains(target)
+      ) return
+      emit('close-sidebar')
+    }
+
+    // close if the esc key is pressed
+    const keyHandler = ({ keyCode }) => {
+      if (!props.sidebarOpen || keyCode !== 27) return
+      emit('close-sidebar')
+    } 
+
+    onMounted(() => {
+      document.addEventListener('click', clickHandler)
+      document.addEventListener('keydown', keyHandler)
+    })
+
+    onUnmounted(() => {
+      document.removeEventListener('click', clickHandler)
+      document.removeEventListener('keydown', keyHandler)
+    })
+
+    watch(sidebarExpanded, () => {
+      localStorage.setItem('sidebar-expanded', sidebarExpanded.value)
+      if (sidebarExpanded.value) {
+        document.querySelector('body').classList.add('sidebar-expanded')
+      } else {
+        document.querySelector('body').classList.remove('sidebar-expanded')
+      }
+    })
+
+    return {
+      trigger,
+      sidebar,
+      sidebarExpanded,
+      currentRoute
+    }
+  },
+}
+</script>
