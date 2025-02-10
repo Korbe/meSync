0fd599d653808c76d030c8692f5c0a4540c1640c
Lukas Korbitsch <korbitschl@gmail.com>
2025-02-10 12:59:30 +0100

    removed package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -18,19 +17,6 @@ export default defineConfig({
                 },
             },
         }),
-        // viteStaticCopy({
-        //     targets: [
-        //       {
-        //         src: 'resources/images/**/*',
-        //         dest: 'images'
-        //       },
-        //       {
-        //         src: 'resources/public/**/*',
-        //         dest: ''
-        //       }
-        //     ],
-        //     watch: true,
-        // }),
         tailwindcss()
     ],
 });
