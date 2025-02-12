5b8d0dfe40afe39d7545a6d8f200b7b8290c3b49
Lukas Korbitsch <korbitschl@gmail.com>
2025-02-12 21:51:47 +0100

    Added VTextarea

---
+++ resources/js/components/VTextarea.vue
@@ -0,0 +1,30 @@
+<template>
+    <textarea
+      :id="id"
+      class="form-textarea w-full focus:border-gray-300"
+      :rows="rows"
+      :placeholder="placeholder"
+      v-model="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    ></textarea>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      modelValue: String,
+      id: {
+        type: String,
+        default: "notes",
+      },
+      rows: {
+        type: Number,
+        default: 4,
+      },
+      placeholder: {
+        type: String,
+        default: "",
+      },
+    },
+  };
+  </script>
