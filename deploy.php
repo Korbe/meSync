83717fb201f31513b440627b6149f4bf43472a45
Lukas Korbitsch <korbitschl@gmail.com>
2025-02-22 22:31:36 +0100

    seperated command in deployer.php

--- deploy.php
+++ deploy.php
@@ -16,16 +16,33 @@ add('writable_dirs', []);
 host('192.168.0.135')
     ->set('ssh_multiplexing', false)
     ->set('remote_user', 'deploy')
-    ->set('deploy_path', '/var/www/html/mesync');
+    ->set('deploy_path', '/var/www/deployment/mesync');
+
+task('composer-install', function () {
+    cd('{{release_path}}');
+    run('composer install');
+    });
+
+task('npm-install', function () {
+    cd('{{release_path}}');
+    run('npm install');
+});
 
 task('build', function () {
     cd('{{release_path}}');
-    run('composer install');
-    run('npm install');
     run('npm run build');
     });
 
-after('deploy:update_code', 'build');
+// task('permissions', function () {
+//     run('chown -R deploy:www-data {{release_path}}');
+//     run('chmod -R 775 {{release_path}}');
+// });
+
+//after('deploy:symlink', 'permissions');
+
+after('deploy:update_code', 'composer-install');
+after('composer-install', 'npm-install');
+after('npm-install', 'build');
 
 //after('deploy:setup', 'deploy:unlock');
 
